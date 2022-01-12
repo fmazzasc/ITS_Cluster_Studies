fa336ba4d6385546bc15ed6e959ff2940d68d448
fmazzasc <fmazzasc@cern.ch>
2022-01-12 11:59:14 +0100

    Update merging script

--- merge_runs.py
+++ merge_runs.py
@@ -19,16 +19,16 @@ if not os.path.exists('part_merging'):
         os.makedirs('part_merging')
 
 
-input_dir = f'alien:/alice/data/2021/OCT/{run_number}/apass2/'
+input_dir = f'/alice/data/2021/OCT/{run_number}/apass2/'
 
 for index,line in enumerate(lines):
     if(index>2): 
             continue
     if line[0:6] == 'o2_ctf':
         input_path = input_dir + line
-        os.system(f' alien_cp -T 32 {input_path}o2match_itstpc.root part_merging/o2match_itstpc.root{index}')
-        os.system(f' alien_cp -T 32 {input_path}o2trac_its.root part_merging/o2trac_its.root{index}')
-        os.system(f' alien_cp -T 32 {input_path}o2clus_its.root part_merging/o2clus_its.root{index}')
+        os.system(f' alien.py cp -T 32 {input_path}o2match_itstpc.root file:part_merging/o2match_itstpc.root{index}')
+        os.system(f' alien.py cp -T 32 {input_path}o2trac_its.root file:part_merging/o2trac_its.root{index}')
+        os.system(f' alien.py cp -T 32 {input_path}o2clus_its.root file:part_merging/o2clus_its.root{index}')
 
 
 os.system(f'hadd {run_number}/o2match_itstpc.root part_merging/o2match_itstpc.root*')
