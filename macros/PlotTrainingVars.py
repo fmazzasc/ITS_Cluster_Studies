36c3a8c47f6fd67f4e98c61586cdb56c5e0de3b4
stefano <stefano.politano@edu.unito.it>
2022-04-22 14:50:47 +0200

    Updating taining variable plot macro with SnPhi cut

--- macros/PlotTrainingVars.py
+++ macros/PlotTrainingVars.py
@@ -58,14 +58,13 @@ def data_prep(data='', betamin=0.6, betamax=0.7, outlabel='', verbose=False): #p
     '''
     function for data preparation
     '''
-    #
     # Load data
-    #_____________________________________________
     df = uproot.open(data)['ITStreeML'].arrays(library='pd')
+    if verbose:
+        print(f'Loading data from {data}')
+        print(f'Data loaded: {df.head()}')
 
-    #
     # Adding columns in dataframe
-    #_____________________________________________
     for i in range(7):
         df[f'ClSizeL{i}'] = np.where(df[f'ClSizeL{i}'] < 0, float("nan"), df[f'ClSizeL{i}'])
     df['L'] = np.arctan(df['tgL'])
@@ -74,22 +73,32 @@ def data_prep(data='', betamin=0.6, betamax=0.7, outlabel='', verbose=False): #p
     df['nSigmaPAbs'] = abs(df['nSigmaP'])
     df.eval('mean_patt_ID = (ClPattIDL0 + ClPattIDL1 + ClPattIDL2 + ClPattIDL3 + ClPattIDL4 + ClPattIDL5 + ClPattIDL6)/7', inplace=True)
     presel = "ClSizeL0 >= 0 and ClSizeL1 >= 0 and ClSizeL2 >= 0 and ClSizeL3 >= 0 and ClSizeL4 >= 0 and ClSizeL5 >= 0 and ClSizeL6 >= 0 and SnPhiL0 >= -1 and SnPhiL1 >= -1 and SnPhiL2 >= -1 and SnPhiL3 >= -1 and SnPhiL4 >= -1 and SnPhiL5 >= -1 and SnPhiL6 >= -1 and  0.05 < p"
+    if verbose:
+        print(f'Preselection: {presel}')
     df_sel = df.query(presel)
 
-    #
     # Particle species df preparation
     #_____________________________________________
     # pi
+    if verbose:
+        print('Preparing pi df')
+        print(f'piTag: {piTag}')
     df_pi = df_sel.query(f'{piTag}', inplace=False)
     df_pi = pd.concat([df_pi, df_sel.query('nSigmaPiAbs < 1 and p > 0.7')])
     df_pi.eval(f'beta = p/sqrt({piMass}**2 + p**2)',  inplace=True)
     df_pi['label'] = 0
     # k
+    if verbose:
+        print('Preparing k df')
+        print(f'kTag: {kTag}')
     df_K = df_sel.query(f'{kTag}', inplace=False)
     df_K = pd.concat([df_K, df_sel.query('nSigmaKAbs < 1 and p > 0.7')])
     df_K.eval(f'beta = p/sqrt({kMass}**2 + p**2)',  inplace=True)
     df_K['label'] = 1
     # p
+    if verbose:
+        print('Preparing p df')
+        print(f'pTag: {pTag}')
     df_P = df_sel.query(f'{pTag}', inplace=False)
     df_P = pd.concat([df_P, df_sel.query('nSigmaPAbs < 1 and p > 0.7')])
     df_P.eval(f'beta = p/sqrt({pMass}**2 + p**2)',  inplace=True)
@@ -98,76 +107,108 @@ def data_prep(data='', betamin=0.6, betamax=0.7, outlabel='', verbose=False): #p
     #
     # Plotting training variables
     #_____________________________________________
-    print('Plotting variable distribution for pions and protons')
-    c1 = TCanvas("c1", "", 1200, 1200)
-    c1.Divide(5, 4)
-
-    Vars = ["ClSizeL0", "ClSizeL1", "ClSizeL2",
-            "ClSizeL3", "ClSizeL4", "ClSizeL5",
-            "ClSizeL6", "meanClsize",
-            "SnPhiL0", "SnPhiL1", "SnPhiL2",
-            "SnPhiL3", "SnPhiL4", "SnPhiL5", "SnPhiL6",  "tgL",
-            "p", "mean_patt_ID"]
+    if verbose:
+        print('Plotting variable distribution for pions and protons')
+    c1 = TCanvas("c1", "", 1800, 1200)
+    c1.Divide(7, 5)
     outFile = TFile(f'TrainVarsPlot{outlabel}.root', 'recreate')
 
+    # Training variables
+    Vars = ["ClSizeL0", "ClSizeL1", "ClSizeL2", "ClSizeL3", "ClSizeL4", "ClSizeL5", "ClSizeL6",
+            "ClPattIDL0", "ClPattIDL1", "ClPattIDL2", "ClPattIDL3", "ClPattIDL4", "ClPattIDL5", "ClPattIDL6",
+            "SnPhiL0", "SnPhiL1", "SnPhiL2", "SnPhiL3", "SnPhiL4", "SnPhiL5", "SnPhiL6",
+            "meanClsize", "tgL", "mean_patt_ID", "p"]
+
+    # Legend and labels
     leg = TLegend(0.4,0.4,0.8,0.8)
-    leg.SetBorderSize(0)
     leg.SetTextSize(0.1)
+    leg.SetFillColor(0)
+    leg.SetBorderSize(0)
+    leg.SetTextFont(42)
     latKS = TLatex()
     latKS.SetNDC()
     latKS.SetTextSize(0.06)
     latKS.SetTextFont(42)
     latKS.SetTextColor(kBlack)
 
+    # Beta preselection
+    if verbose:
+        print(f'Preselection: {betamin} < beta < {betamax}')
     df_pi_sel = df_pi.query(f'{betamin} < beta < {betamax}')
     df_p_sel = df_P.query(f'{betamin} < beta < {betamax}')
 
+    # Plotting
+    #_____________________________________________
     with alive_bar(len(Vars), title="Plotting variables") as bar:
         for i, _ in enumerate(Vars):
             pad = c1.cd(i+1)
             pad.SetLogy()
-            if 0 <= i <= 7: # ClSize & meanClsize
+
+            # Binnning and histograms
+            if 'ClSize' in Vars[i]: # ClSize
                 minvar = 0.5
                 maxvar = 10.5
                 bins = 10
                 latX = 0.2
                 latY = 0.2
-            elif (i > 7 and i <= 15): # SnPhi & tgL
+            elif ('SnPhi' in Vars[i]) or ('tgL' in Vars[i]): # SnPhi and tgL
                 minvar = -1.
                 maxvar = 1.
                 bins = 2000
                 latX = 0.15
                 latY = 0.85
-            elif i == 16:  # p
+            elif 'p' == Vars[i]: # p
                 minvar = 0.
                 maxvar = 2.
                 bins = 2000
                 latX = 0.7
                 latY = 0.8
-            else:   # mean_patt_ID
+            else:   # patt_ID
                 minvar = min(min(df_pi[Vars[i]]), min(df_P[Vars[i]]))
                 maxvar = max(max(df_pi[Vars[i]]), max(df_P[Vars[i]]))
                 bins = int((maxvar - minvar)*100)
-                latX = 0.7
+                latX = 0.6
                 latY = 0.8
             hVarpi = TH1F(f'h{Vars[i]}_pi', f';{Vars[i]}; counts', bins, minvar, maxvar)
             hVarp = TH1F(f'h{Vars[i]}_p', f';{Vars[i]}; counts', bins, minvar, maxvar)
             SetHistStyle(hVarpi, colors[0], markers[0], xtitle=f'{Vars[i]}', ytitle='counts')
             SetHistStyle(hVarp, colors[2], markers[2], xtitle=f'{Vars[i]}', ytitle='counts')
-            for pi in df_pi_sel[f'{Vars[i]}']:
+
+            # Filling histograms
+            # pions
+            if verbose:
+                print(f'Filling histogram for {Vars[i]}')
+            if ('ClSizeL' in Vars[i]):
+                df_pi_sel_var = df_pi_sel.query(f'0.< SnPhiL{i} < 0.2', inplace=False)
+            elif ('SnPhiL' in Vars[i]):
+                df_pi_sel_var = df_pi_sel.query(f'0.< {Vars[i]} < 0.2', inplace=False)
+            else: 
+                df_pi_sel_var = df_pi_sel
+            for pi in (df_pi_sel_var[f'{Vars[i]}']):
                 hVarpi.Fill(pi)
-            for p in df_p_sel[f'{Vars[i]}']:
+            # protons
+            if ('ClSizeL' in Vars[i]):
+                df_p_sel_var = df_p_sel.query(f'0.< SnPhiL{i} < 0.2', inplace=False)
+            elif ('SnPhiL' in Vars[i]):
+                df_p_sel_var = df_p_sel.query(f'0.< {Vars[i]} < 0.2', inplace=False)
+            else: 
+                df_p_sel_var = df_p_sel
+            for p in df_p_sel_var[f'{Vars[i]}']:
                 hVarp.Fill(p)
-            KStest = hVarpi.KolmogorovTest(hVarp) # KS test
+
+            # K-S test
+            KStest = hVarpi.KolmogorovTest(hVarp)
             if verbose:
                 print(f'Kolmogorov-Smirnov test for pions and protons - variable: {Vars[i]} --> KS-pion: {hVarpi.KolmogorovTest(hVarp)} - KS-proton: {hVarp.KolmogorovTest(hVarpi)}\n')
-            hVarpi.DrawNormalized('e')
+            
+            # Drawing and saving histograms
+            hVarpi.DrawNormalized('esame')
             hVarp.DrawNormalized('esame')
             latKS.DrawLatex(latX, latY, f'KS={round(KStest, 4)}')
             if i+1 == len(Vars):
                 leg.SetHeader(f'{betamin} < #beta < {betamax}')
-                leg.AddEntry(hVarpi,"#pi","l")
-                leg.AddEntry(hVarp,"p","l")
+                leg.AddEntry(hVarpi,"#pi","lp")
+                leg.AddEntry(hVarp,"p","lp")
                 c1.cd(len(Vars)+1)
                 leg.Draw()
             c1.Update()
@@ -176,14 +217,19 @@ def data_prep(data='', betamin=0.6, betamax=0.7, outlabel='', verbose=False): #p
             bar()
     c1.Write()
     outFile.Close()
+
+    # Saving png and pdf files
     for format in ('png', 'pdf'):
+        if verbose:
+            print(f'Saving plot in {format} format')
         c1.SaveAs(f'TrainVarsPlot{outlabel}.{format}')
-    print('Training variables plot completed.\n')
+    if verbose:
+        print('Training variables plot completed.\n')
 
 def main():
     data = '/data/fmazzasc/its_data/TreePID_505658.root'
-    outlabel = ''
-    data_prep(data, betamin=0.6, betamax=0.7, outlabel=outlabel)
+    outlabel = 'snphicut'
+    data_prep(data, betamin=0.7, betamax=0.8, outlabel=outlabel)
     input('Press enter to exit')
     sys.exit()
 
